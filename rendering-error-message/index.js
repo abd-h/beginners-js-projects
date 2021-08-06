74a7797ba0858bd6d9d55f2140fc438fa1103fdd
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-08-06 01:15:32 +0100

    Added README.md file
    description

---
+++ rendering-error-message/index.js
@@ -0,0 +1,7 @@
+// setup for error message
+let paragraphError = document.getElementById("error");
+let errorMsg = "Something went wrong, please try again"
+function purchase(){
+    paragraphError.textContent += errorMsg;
+    return errrorMsg = "";
+}
