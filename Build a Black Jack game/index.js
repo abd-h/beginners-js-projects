2dc9b03d9e24fc0b0b32d6e70b151fea093df7cc
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-08-08 01:55:56 +0100

    Added Blackjack game

--- Build a Black Jack game/index.js
+++ Build a Black Jack game/index.js
@@ -1,7 +1,8 @@
   
     //Building blackjack Game
    let firstCard = 10;
-   let secondCard = 11;
+   let secondCard = 10;
+   let cards = [firstCard, secondCard]// array - ordered list of items.
     let total = firstCard + secondCard;
 
     // Add the hasBlackJack variable
@@ -10,18 +11,67 @@
     //added isAlive
     let isAlive = true;
 
+    //Adding the message variable
+    let message = "";
 
-    (total < 21)
-                     ? console.log(`Do you want to draw new card 🤔`) 
-                     :(total === 21)
-                     ? console.log("Congratulations 😎")
-                     ? hasBlackJack  = true
-                     : console.log("Game is Over")
-                     :isAlive = false;
+    // Created message Variable for displaying message
+    let messageEl = document.getElementById("message-el");
 
+    // Created sumEl for displaying sum by using querySelector
+    //let sumEl = document.querySelector("#sum-el")
+
+    // Created sumEl for displaying sum
+    let sumEl = document.getElementById("sum-el")
+
+    //Created cardsEl variable for displaying cards tally
+    let cardsEl = document.getElementById("cards-el");
+
+    
+
+
+    // (total <= 20)? message = `Want to play a round ?`
+    // :(total === 21)? (message += `Congratulations 😎`,hasBlackJack  = true)
+    // : (message += `Game is Over`, isAlive = false);
     console.log(total);
     console.log(hasBlackJack);
     console.log(isAlive);
+    console.log(message);
+
+    // Creating startGame function for the Start Game button
+function startGame(){
+    renderGame();
+}
+
+// Renamed  startGame to renderGame
+  function renderGame() {
+      // render out firstCard and secondCard  
+      cardsEl.textContent = `Cards: ${cards[0]} ${cards[1]}`;
+      sumEl.textContent = `Sum: ${total}`
+     if(total <=20) {
+         message = `doyou want draw new `;
+     }
+     else if (total === 21) {
+         message =`congratulations`;
+         hasBlackJack = true;
+     }
+     else{
+         message = `Game is Over`;
+         isAlive = false
+     }
+
+    messageEl.textContent = message;
+  }
+
+  //New card function
+  function newCard(){
+      console.log("Drawing a new card from the deck");
+      //add to the sum when newCard is clicked
+      let thirdCard = 1  ;
+      total += thirdCard;
+      renderGame();
+  }
 
 
-
+/* (total <= 20)? message = `Want to play around ?`
+    :(total === 21)? (message += `Congratulations`, hasBlackJack  = true)
+    : (message += `Game is Over`, isAlive = false);*/
