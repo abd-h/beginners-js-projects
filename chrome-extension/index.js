bb6ac360762c9ddfb6349e4c804c7d9988582322
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2021-08-22 23:09:55 +0100

    Added innerHTML practice

--- chrome-extension/index.js
+++ chrome-extension/index.js
@@ -1,14 +1,20 @@
-// Creating myLeads array and inputEl
 let myLeads = [];
 const inputEl= document.getElementById("input-el");
 let inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
+localStorage.setItem("myLeads",  "www.awsomeleads.com");
+localStorage.getItem("myLeads")
+console.log(localStorage.getItem("myLeads") );
+localStorage.clear("myLeads")
 
 inputBtn.addEventListener( "click",  function() {
     myLeads.push(inputEl.value)
     inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    
+    
     renderLeads();
- 
+    console.log(localStorage.getItem("myLeads"));
     });
 
     // 1. wrapping the code below in a renderLeads()  function
@@ -37,9 +43,4 @@ inputBtn.addEventListener( "click",  function() {
 
     
 
-        // Buy button
-        const container = document.getElementById("container")
-        container.innerHTML += `<button class="button"  onclick='buy()'>Buy!</button>`;
-function buy(){
-    container.innerHTML += `<p class="le">Thank you for buying</p>`
-}
+        
--- chrome-extension/practice-sheet.js
+++ chrome-extension/practice-sheet.js
@@ -22,3 +22,27 @@
     let nEmail = `Hi ${recepient} 
                         How is it going? cheers ${sender}`;
     console.log(nEmail);
+
+    // Buy button
+        const container = document.getElementById("container")
+        container.innerHTML += `<button class="button"  onclick='buy()'>Buy!</button>`;
+function buy(){
+    container.innerHTML += `<p class="le">Thank you for buying</p>`
+}
+
+// Your first local storage
+let myLeads2 = `["www.awsomeleads.com"]`;
+localStorage.setItem("myLeads",  "www.awsomeleads.com");
+localStorage.getItem("myLeads")
+console.log(localStorage.getItem("myLeads") );
+localStorage.clear("myLeads")
+
+
+//storing arrays in localStorage
+myLeads2 = JSON.parse(myLeads2);
+myLeads2.push(" www.apple.com");
+console.log(myLeads2);
+
+// arrays stringfied;
+myLeads2 = myLeads2.toString(myLeads2);
+console.log(typeof myLeads2);
